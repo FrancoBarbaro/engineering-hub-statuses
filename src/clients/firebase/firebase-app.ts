42e7e3b2d7446658d90471956d9a6a0c729d2a7f
Franco Barbaro <franbarbaro1@gmail.com>
2023-10-29 22:44:10 -0500

    added firebase app

---
+++ src/clients/firebase/firebase-app.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSSm0H_g1AUjZFWxSGvCGsLMVQ7l3y81g",
+  authDomain: "engineering-hub-api.firebaseapp.com",
+  databaseURL: "https://engineering-hub-api-default-rtdb.firebaseio.com",
+  projectId: "engineering-hub-api",
+  storageBucket: "engineering-hub-api.appspot.com",
+  messagingSenderId: "375536364338",
+  appId: "1:375536364338:web:b100abfbfd14128d7f852b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
