8526f4865fbb6e944132bb75f3cb5163f7c38929
Franco Barbaro <franbarbaro1@gmail.com>
2023-11-26 05:17:28 -0600

    added Head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: FC = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Home | Engineering Hub Statuses</title>
         <meta name="description" content="home page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
--- src/pages/professors/[profName].tsx
+++ src/pages/professors/[profName].tsx
@@ -1,6 +1,7 @@
 import { LoadingSpinner } from "@/components/loading-spinner";
 import { ProfessorPage } from "@/components/professor-page";
 import { useFetchProfessorInfo } from "@/hooks/use-fetch-professor-info";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 const ProfName = () => {
@@ -8,14 +9,24 @@ const ProfName = () => {
   const { profName } = router.query;
   const { data, changeSwrData } = useFetchProfessorInfo(profName);
 
-  return data ? (
-    <ProfessorPage
-      info={data}
-      hyphenatedName={typeof profName === "string" ? profName : ""}
-      changeSwrData={changeSwrData}
-    />
-  ) : (
-    <LoadingSpinner />
+  return (
+    <>
+      <Head>
+        <title>{data ? data.name : "Loading"} | Engineering Hub Statuses</title>
+        <meta name="description" content="professor page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {data ? (
+        <ProfessorPage
+          info={data}
+          hyphenatedName={typeof profName === "string" ? profName : ""}
+          changeSwrData={changeSwrData}
+        />
+      ) : (
+        <LoadingSpinner />
+      )}
+    </>
   );
 };
 
